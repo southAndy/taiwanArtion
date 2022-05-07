2559739ce2defc5a0630ceb38c9e3047fb2ad4b5
southAndy <zatchbell.1206@gmail.com>
2022-05-07 17:27:52 +0800

    feat:create basic router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-link to="/search">search-icon </router-link>
+    <router-link to="/search-map">地圖找展</router-link>
+    <router-link to="/shared">分享展覽</router-link>
+    <!-- <router-link to="/shared">分享展覽</router-link> -->
+    <router-view />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  components: {},
+  created() {
+    console.log(this.$route);
   },
 };
 </script>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "SearchView",
+  data() {
+    return {};
+  },
+  methods: {
+    tempSwitchPage() {
+      this.$router.push({ name: "ResultView" });
+    },
+  },
+};
+</script>
+<template>
+  <div class="search">
+    <!-- todo 以動態component切換 -->
+    <router-link :to="{ name: 'DetailView' }">不限區域</router-link>
+    <router-link :to="{ name: 'DetailView' }">單位</router-link>
+    <router-link :to="{ name: 'DetailView' }">日期</router-link>
+    <router-view></router-view>
+    <button @click="tempSwitchPage">確定</button>
+  </div>
+</template>
