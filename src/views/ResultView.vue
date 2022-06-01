e6684ceba84210644df1612402e100d5c3024b49
southAndy <zatchbell.1206@gmail.com>
2022-06-01 16:38:56 +0800

    feat:refactor result layout

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -1,12 +1,30 @@
 <script>
+import Result_Card from "@/components/Cards/Result.vue";
+import Search from "@/components/Search.vue";
+import Navbar_select from "@/components/Navbar/Select.vue";
+
 export default {
   name: "ResultView",
+  components: {
+    Result_Card,
+    Search,
+    Navbar_select,
+  },
 };
 </script>
 <template>
-  <div>
-    Result page
-    <router-link :to="{ name: 'DetailView' }">Card</router-link>
-    <router-view></router-view>
+  <div class="result">
+    <Search />
+    <Navbar_select />
+    <h3>臺北市,共500件展覽</h3>
+    <router-link :to="{ name: 'ResultView' }">card</router-link>
+    <router-view>
+      <Result_Card />
+    </router-view>
   </div>
 </template>
+<style>
+.result {
+  padding: 16px;
+}
+</style>
