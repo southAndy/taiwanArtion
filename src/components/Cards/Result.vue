ce828653e4eabeb8cde2f791fdde994fb87d06a6
southAndy <zatchbell.1206@gmail.com>
2022-06-01 16:37:31 +0800

    feat:modulize components

---
+++ src/components/Cards/Result.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="result_card">
+    <div class="card_image">
+      <img src="@/assets/images/Frame89.png" alt="" />
+    </div>
+    <div class="card_content">
+      <div class="title">
+        <!-- todo api replace -->
+        <h3>會動的文藝復興</h3>
+        <p>2020/03/21-4/20</p>
+      </div>
+      <div class="content">
+        <i class="content_icon">icon</i>
+        <p class="content_place">台南市,奇美博物館</p>
+        <p class="content_price">$300</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Result",
+};
+</script>
+<style lang="scss" scoped>
+@use "@/assets/scss/base/reset";
+.result_card {
+  background: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  width: 360px;
+  border-radius: 10px;
+  margin: 0 auto;
+
+  .card_content {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+
+    .title {
+      align-self: center;
+      h3 {
+        font-size: 18px;
+        margin: 0;
+      }
+      p {
+        font-size: 12px;
+        text-align: start;
+      }
+    }
+    .content {
+      font-size: 12px;
+      &_place {
+        display: inline-block;
+      }
+      &_price {
+        background: #be875c;
+        color: white;
+        border-radius: 4px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/buttons/sendResult.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="sendResult" class="send">確定</button>
+</template>
+<script>
+export default {
+  name: "SendResult",
+  props: {
+    selected: String,
+  },
+  methods: {
+    sendResult() {
+      this.$router.push({
+        name: "ResultView",
+        query: { selected: this.selected },
+      });
+    },
+  },
+};
+</script>
+<style>
+.send {
+  width: 100%;
+  height: 36px;
+  border-radius: 4px;
+  border: none;
+  margin-top: 35px;
+}
+</style>
