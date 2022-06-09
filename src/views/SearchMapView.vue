6228e025646157981a49e23a8ad6272b59a0dc37
southAndy <zatchbell.1206@gmail.com>
2022-06-09 09:31:45 +0800

    feat:install leaflet

--- src/views/SearchMapView.vue
+++ src/views/SearchMapView.vue
@@ -1,4 +1,6 @@
 <script>
+import L from "leaflet";
+
 export default {
   name: "searchMapView",
   //   setup() {},
@@ -6,6 +8,21 @@ export default {
     search() {
       //   this.$route.push()
     },
+  },
+  mounted: {
+    //設定中心點座標
+    // var map = L.map("map").setView([22.99975613908633, 120.21350714331682], 19);
+    var map = L.map("map", {
+      center: [22.99975613908633, 120.21350714331682],
+      zoom: 19,
+    });
+    // //設定引用圖層來源:openstreet
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+    // //釘選當前的座標位置
+    L.marker([22.99975613908633, 120.21350714331682]).addTo(map);
   },
 };
 </script>
