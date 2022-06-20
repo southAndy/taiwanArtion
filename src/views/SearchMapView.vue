f87ac561204705af6686e80ea4f378b4fc08cc66
southAndy <zatchbell.1206@gmail.com>
2022-06-21 00:38:12 +0800

    feat:testing leaflet

--- src/views/SearchMapView.vue
+++ src/views/SearchMapView.vue
@@ -12,7 +12,6 @@ export default {
   mounted() {
     //設定中心點座標
     // var map = L.map("map").setView([22.99975613908633, 120.21350714331682], 19);
-<<<<<<< Updated upstream
     // var map = L.map("map", {
     //   center: [22.99975613908633, 120.21350714331682],
     //   zoom: 19,
@@ -24,19 +23,6 @@ export default {
     // }).addTo(map);
     // //釘選當前的座標位置
     // L.marker([22.99975613908633, 120.21350714331682]).addTo(map);
-=======
-    let map = L.map("map", {
-      center: [22.99975613908633, 120.21350714331682],
-      zoom: 19,
-    });
-    // //設定引用圖層來源:openstreet
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-    // 釘選當前的座標位置
-    L.marker([22.99975613908633, 120.21350714331682]).addTo(map);
->>>>>>> Stashed changes
   },
 };
 </script>
