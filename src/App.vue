4b48398d07a411eb7c476eeeafc72282de81c6a3
southAndy <zatchbell.1206@gmail.com>
2022-06-24 19:53:24 +0800

    feat:adding components:OffCanvas,navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,4 @@
 <template>
-  <nav class="home_menu">
-    <li>
-      <img src="@/assets/images/↳Color.png" alt="點擊選單" />
-    </li>
-    <li>
-      <router-link to="/search-map"> 附近展覽 </router-link>
-    </li>
-    <li>
-      <router-link to="/search">
-        <img src="@/assets/images/icon.png" alt="搜尋" />
-      </router-link>
-    </li>
-  </nav>
   <router-view />
 </template>
 
@@ -25,10 +12,5 @@
   text-align: center;
   color: #2c3e50;
   overflow: hidden;
-}
-.home_menu {
-  display: flex;
-  justify-content: space-between;
-  padding: 16px;
 }
 </style>
