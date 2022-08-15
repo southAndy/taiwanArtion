cac7dc2a257ee236f1a398cbf9c37273e442c564
southAndy <zatchbell.1206@gmail.com>
2022-08-15 16:28:17 +0800

    refactor:remove useless kits

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+// ant-design
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
-import drag from "v-drag";
 
 //
 
-createApp(App).use(Antd).use(drag).use(store).use(router).mount("#app");
+createApp(App).use(Antd).use(store).use(router).mount("#app");
