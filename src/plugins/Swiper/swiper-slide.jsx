93bfa52d78f3bd06962f33a6584abf7ae0d93c7c
southAndy <zatchbell.1206@gmail.com>
2023-01-18 10:15:39 +0800

    init Swiper's component

---
+++ src/plugins/Swiper/swiper-slide.jsx
@@ -0,0 +1,25 @@
+import { Navigation,  A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+export default () => {
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
