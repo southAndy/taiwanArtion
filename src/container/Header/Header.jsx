ae108cf1bead8310d842d8d7ac82e5681bd81825
southAndy <zatchbell.1206@gmail.com>
2023-03-16 14:50:58 +0800

    feat:Header - add props, and modify some component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,10 @@
 //本身設定
 import './Header.scss'
-//引入元件
+import { useState } from 'react'
+
+
 import Dropdown from '../../component/Dropdown/Dropdown'
+import DateSelecter from '../../plugins/date-picker'
 
 import {Link} from "react-router-dom"
 
@@ -10,18 +13,24 @@ import searchIcon from "../../assets/images/search-icon.png"
 import dropdownIcon from "../../assets/images/Vector.png"
 import fakeUserIcon from '../../assets/images/Ellipse22.png'
 import notifyIcon from "../../assets/images/notify159.png"
+import Input from '../../component/Input'
 
 const Header = ({setClick})=>{
+    let [isShowModal,setShowMoal]=useState(false)
+    //? 存多選的狀態
+    let [selectedParams,setParams]=useState([])
+
     return (
         <header className='header-container'>
-            <a className='logo' href="##">
+            <Link to={'/'} className="logo">
                 <img src="/src/assets/images/logo-05 3.png" alt="網站logo" />
-            </a>
+            </Link>
             <div className='filter filter-box'>
-                <Dropdown className='filter-item' menu={'輸入展覽名稱'} icon={dropdownIcon} />
+                <Input/>
+                <DateSelecter isShowModal={isShowModal}/>
                 <Dropdown className='filter-item' menu={'選擇地區'} icon={dropdownIcon}/>
                 <Dropdown className='filter-item' menu={'選擇展區'} icon={dropdownIcon}/>
-                <Dropdown className='filter-item' menu={'開始日期'} icon={dropdownIcon}/>
+                <Dropdown className='filter-item' menu={'開始日期'} icon={dropdownIcon} isShowModal={isShowModal} setShowModal={setShowMoal}/>
                 <Dropdown className='filter-item' menu={'結束日期'} icon={dropdownIcon}/>
                 <div className='filter-item_button'>
                     <img src={searchIcon} alt="搜尋樣式" />
