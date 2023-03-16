3ff4fde1829c4b97b5c82b6e889c4b1e1a9496df
southAndy <zatchbell.1206@gmail.com>
2023-03-16 12:18:43 +0800

    feat:modify mui's datePicker style

--- src/plugins/date-picker.jsx
+++ src/plugins/date-picker.jsx
@@ -2,13 +2,22 @@ import { CalendarPicker } from "@mui/x-date-pickers"
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-export default function DateSelecter(){
+
+//todo 改版為styled
+import styled,{css} from "styled-components";
+import './date-picker.scss';
+
+
+// const styledDatePicker = styled()
+export default function DateSelecter({isShowModal}){
     function handleOnChangeDate(date){
         console.log(date)
     }
     return (
+        <div className={isShowModal?'date-show':'date'}>
             <LocalizationProvider dateAdapter={AdapterDayjs} >
                 <CalendarPicker onChange={handleOnChangeDate} value={null}/>
             </LocalizationProvider>
+        </div>
     )
 }
