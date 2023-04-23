da0699abb1768552873764317c5c3de114ee0713
southAndy <zatchbell.1206@gmail.com>
2023-04-23 21:00:24 +0800

    feat(Dropdown):add new props to dropdown

--- src/component/Dropdown.tsx
+++ src/component/Dropdown.tsx
@@ -1,19 +1,17 @@
 import styled from "@emotion/styled";
 
-interface Props {
-  dropName: string;
-  dropMenu: string[];
-  isShowDrop: boolean;
-  updateDrop: Function;
-}
-
 const DropdownContainer = styled.div`
+  position: relative;
   display: flex;
+  justify-content: center;
   align-items: center;
+  font-size: 14px;
   width: 100px;
   height: 100px;
   max-width: 124px;
+  box-shadow: 1px 0px 0px #dadada;
   height: 32px;
+  cursor: pointer;
 `;
 const DropdownTitle = styled.p`
   color: #b0b0b0;
@@ -21,23 +19,63 @@ const DropdownTitle = styled.p`
 
 const DropMenuList = styled.div`
   display: ${(props: { showList: Boolean }) =>
-    props.showList ? "flex" : "none"};
+    props.showList ? "block" : "none"};
+  width: 128px;
+  max-height: 232px;
+  position: absolute;
+  z-index: 2;
+  left: 0%;
+  top: 125%;
+  background: #ffffff;
+  box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
 `;
 const DropMenu = styled.div`
-  display: flex;
-  width: 100%;
+  font-size: 14px;
+  text-align: center;
+  &:hover {
+    color: #be875c;
+  }
 `;
 
-const Dropdown = ({ dropMenu, dropName, isShowDrop, updateDrop }: Props) => {
+type Props = {
+  dropName: string;
+  dropMenu: string[];
+  isShowDrop: boolean;
+  updateDrop: Function;
+  selectedOption: Function;
+  keyword: { type: string };
+};
+
+const Dropdown = ({
+  dropMenu,
+  dropName,
+  isShowDrop,
+  updateDrop,
+  selectedOption,
+  keyword,
+}: Props) => {
   return (
     <>
       <DropdownContainer>
         <DropdownTitle onClick={() => updateDrop(!isShowDrop)}>
-          {dropName}
+          {keyword?.type ? keyword?.type : dropName}
         </DropdownTitle>
         <DropMenuList showList={isShowDrop}>
-          {dropMenu.map((menu: string, index: number) => (
-            <DropMenu key={index}>{menu}</DropMenu>
+          {dropMenu?.map((menu: string, index: number) => (
+            <DropMenu
+              onClick={() =>
+                selectedOption((payload: { type: string }) => {
+                  payload.type = menu;
+                  //開關下拉
+                  updateDrop(false);
+                  return payload;
+                })
+              }
+              key={index}
+            >
+              {menu}
+            </DropMenu>
           ))}
         </DropMenuList>
       </DropdownContainer>
