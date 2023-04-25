cf3925951dcea82668ecac81741b37d8090c747d
southAndy <zatchbell.1206@gmail.com>
2023-04-25 23:27:41 +0800

    feat(Page-detail) adjust layout of detail's page

--- src/page/deatil-page.tsx
+++ src/page/deatil-page.tsx
@@ -1,8 +1,45 @@
 import { useState, useEffect, useMemo } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 import "./detail-page.scss";
+import styled from "@emotion/styled";
+
+const DetailTitle = styled.h3`
+  border-left: 10px solid #986f4f;
+  // border-radius: 10px 10px;
+  font-family: "Noto Sans TC";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 35px;
+  color: #986f4f;
+`;
+const DetailContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 40px;
+`;
+const DetailBanner = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+const DetailOption = styled.p`
+  padding: 20px;
+  cursor: pointer;
+  border-bottom: ${(props: { isActive: boolean }) =>
+    props.isActive ? "5px #986F4F solid" : "none"};
+  color: ${(props: { isActive: boolean }) =>
+    props.isActive ? "#986F4F" : "black"};
+`;
+
+const BannerImage = styled.img`
+  width: 1356.75px;
+  height: 711px;
+  border-radius: 10px;
+  position: relative;
+`;
 
 type exhibitionType = {
   title: string;
@@ -15,17 +52,10 @@ type exhibitionType = {
   showUnit: string;
 };
 
-// const detailTitle = styled.h3`
-//   color: #986f4f;
-//   font-style: normal;
-//   font-weight: 700;
-//   font-size: 24px;
-// `;
-
 export default function DetailPage() {
   let [exhibition, setExhibition] = useState<exhibitionType[]>([]);
   let params = useParams();
-
+  let [currentOption, setCurrentOption] = useState<number>(0);
   //todo recall api to get page's data
   useEffect(() => {
     async function fetchData() {
@@ -45,34 +75,31 @@ export default function DetailPage() {
   let currentData = useMemo(() => {
     return exhibition.filter((item) => item.UID === params.dataID);
   }, [exhibition]);
+  // let getCurrentOption = useMemo(()=>,[currentOption])
 
   return (
     <>
-      <section>
-        <div className="banner">
-          <img className="banner-image" src={currentData[0]?.imageUrl} />
-        </div>
-        <p className="banner-title">{currentData[0]?.title}</p>
-      </section>
-      <section className="menu">
-        <div className="menu-item">展覽總覽</div>
-        <div className="menu-item">展覽簡介</div>
-        <div className="menu-item">展覽票價</div>
-        <div className="menu-item">展覽地點</div>
-      </section>
-      <article>
-        <section className="info">
-          <h3 className="info-title">展覽總覽</h3>
-          <div>
-            <div className="info-menu">展覽類別</div>
-            <div className="info-menu">展覽日期</div>
-            <div className="info-menu">營業時間</div>
-            <div className="info-menu">主辦單位</div>
-            <div className="info-menu">展覽官網</div>
-            <div className="info-menu">展覽電話</div>
+      <DetailContainer>
+        <DetailBanner>
+          <Link to={"/"}>返回首頁</Link>
+          <div className="banner">
+            <img className="banner-image" src={currentData[0]?.imageUrl} />
           </div>
+          <p className="banner-title">{currentData[0]?.title}</p>
+        </DetailBanner>
+        <section className="menu">
+          <DetailOption isActive={true}>展覽總覽</DetailOption>
+          <DetailOption isActive={false}>展覽簡介</DetailOption>
+          <DetailOption isActive={false}>展覽票價</DetailOption>
+          <DetailOption isActive={false}>地點總覽</DetailOption>
         </section>
-      </article>
+        <article>
+          <section className="info">
+            <DetailTitle>展覽簡介</DetailTitle>
+            <p>{currentData[0]?.descriptionFilterHtml}</p>
+          </section>
+        </article>
+      </DetailContainer>
     </>
   );
 }
