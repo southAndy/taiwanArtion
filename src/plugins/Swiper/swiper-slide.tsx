8a41b66f93997bfa11ec494312ff0c9189d43236
southAndy <zatchbell.1206@gmail.com>
2023-05-25 16:24:13 +0800

    feat(swiper) modify navigation icon's position

--- src/plugins/Swiper/swiper-slide.tsx
+++ src/plugins/Swiper/swiper-slide.tsx
@@ -1,6 +1,7 @@
 import { Navigation, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // customer css
 import "./Swipers.scss";
@@ -11,27 +12,29 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 export default ({ isLoading = true, data = [] }) => {
+  // todo 將 navigation 樣式調整
   return (
     <Swiper
       className="swipers"
       modules={[Navigation, A11y]}
       spaceBetween={50}
       slidesPerView={3}
-      navigation
+      navigation={!isLoading}
       loop={true}
     >
       {data.map((data: { id: string; imageUrl: string }, index: number) => {
         return (
           <SwiperSlide key={index} className="slide">
-            <a className={data.imageUrl ? "" : "skeleton"} href="##">
-              <div className="slide-image">
-                {/* todo:補上假圖 */}
-                <img
-                  src={data.imageUrl || ""}
-                  className={isLoading ? "hide" : "slide-image"}
-                />
-              </div>
-            </a>
+            <Link
+              className={isLoading ? "skeleton" : "slide-image"}
+              to={"/detail"}
+            >
+              {/* todo:補上假圖 */}
+              <img
+                src={data.imageUrl || ""}
+                className={isLoading ? "hide" : "slide-image"}
+              />
+            </Link>
           </SwiperSlide>
         );
       })}
