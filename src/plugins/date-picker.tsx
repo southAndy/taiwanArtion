bd6e1d7695c1228eaef26de97a491b871dd0b358
southAndy <zatchbell.1206@gmail.com>
2023-06-09 17:54:44 +0800

    revert(mui): remove mui's date-picker

--- src/plugins/date-picker.tsx
+++ src/plugins/date-picker.tsx
@@ -1,13 +1,20 @@
-import { CalendarPicker } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+// import { CalendarPicker } from "@mui/x-date-pickers";
+// import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
+import { LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 //todo 改版為styled
 import "./date-picker.scss";
 
 type Props = {
   isShowModal: boolean;
+};
+type dateRange = {
+  start: string;
+  end: string;
 };
 
 // const styledDatePicker = styled()
@@ -15,7 +22,7 @@ export default function DateSelecter({ isShowModal }: Props) {
   return (
     <div className={isShowModal ? "date-show" : "date"}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <CalendarPicker onChange={handleOnChangeDate} value={null} />
+        <DateRangePicker localeText={{ start: "開始日期", end: "結束日期" }} />
       </LocalizationProvider>
     </div>
   );
