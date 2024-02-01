e13d80b679c7e158cc79fb92a0bcad93f966b16e
southAndy <zatchbell.1206@gmail.com>
2024-02-01 12:02:22 +0800

    feat(images): create image's collection js

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+import searchIcon from '../../assets/images/search-icon.png'
+import dropdownIcon from '../../assets/images/Vector.png'
+import fakeUserIcon from '../../assets/images/Ellipse22.png'
+import notifyIcon from '../../assets/images/notify159.png'
+
+export { searchIcon, dropdownIcon, fakeUserIcon, notifyIcon }
