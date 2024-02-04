23ea40e53107489dea355c3b60d576a22bd9d2d6
southAndy <zatchbell.1206@gmail.com>
2024-02-04 20:49:26 +0800

    chore(assert): add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,5 +3,39 @@ import dropdownIcon from '../../assets/images/Vector.png'
 import fakeUserIcon from '../../assets/images/Ellipse22.png'
 import notifyIcon from '../../assets/images/notify159.png'
 import logoIcon from '../../assets/images/logo.png'
+import categoryicon1 from '../../assets/images/categoryicon1.png'
+import categoryicon2 from '../../assets/images/categoryicon2.png'
+import categoryicon3 from '../../assets/images/categoryicon3.png'
+import categoryicon4 from '../../assets/images/categoryicon4.png'
+import categoryicon5 from '../../assets/images/categoryicon5.png'
+import categoryicon6 from '../../assets/images/categoryicon6.png'
+import categoryicon7 from '../../assets/images/categoryicon7.png'
+import categoryicon8 from '../../assets/images/categoryicon8.png'
+import categoryicon9 from '../../assets/images/categoryicon9.png'
+import categoryicon10 from '../../assets/images/categoryicon10.png'
+import hotBg from '../../assets/images/hot-bg.png'
+import locationIcon from '../../assets/images/locationicon.png'
+import loveIcon from '../../assets/images/loveicon.png'
+import sampleBg from '../../assets/images/sample.png'
 
-export { searchIcon, dropdownIcon, fakeUserIcon, notifyIcon, logoIcon }
+export {
+   searchIcon,
+   dropdownIcon,
+   fakeUserIcon,
+   notifyIcon,
+   logoIcon,
+   categoryicon1,
+   categoryicon2,
+   categoryicon3,
+   categoryicon4,
+   categoryicon5,
+   categoryicon6,
+   categoryicon7,
+   categoryicon8,
+   categoryicon9,
+   categoryicon10,
+   hotBg,
+   locationIcon,
+   loveIcon,
+   sampleBg,
+}
