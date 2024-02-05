6c69fa06500aa7dba2b11e67ff0c0cb59ec5005d
southAndy <zatchbell.1206@gmail.com>
2024-02-05 14:50:56 +0800

    feat(component): add modal component

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,1 +1,66 @@
+import styled from 'styled-components'
+import { useEffect } from 'react'
 
+const StyledModalBackground = styled.div`
+   display: ${({ showModal }) => (showModal ? 'block' : 'none')};
+   position: absolute;
+   top: 10%;
+   left: 0;
+   background-color: rgba(0, 0, 0, 0.2);
+   width: 100vw;
+   height: 100vh;
+   z-index: 100;
+`
+const StyledModalContent = styled.div`
+   background: ${({ bgColor }) => bgColor || '#fff'};
+   height: ${({ height }) => height || '295px'};
+   width: ${({ width }) => width || '100%'};
+   border-radius: ${({ borderRadius }) => borderRadius || '0'};
+   position: absolute;
+   top: ${({ top }) => top || '0%'};
+   bottom: ${({ bottom }) => bottom || '0%'};
+   left: ${({ left }) => left || '0%'};
+   right: ${({ right }) => right || '0%'};
+`
+
+const Modal = ({
+   children,
+   isShow,
+   setShow,
+   position = { l: 0, r: 0, t: 0, b: 0 },
+   size = { w: 0, z: 0 },
+}) => {
+   useEffect(() => {
+      if (isShow) {
+         document.body.style.overflow = 'hidden'
+      } else {
+         document.body.style.overflow = 'auto'
+      }
+   }, [isShow])
+
+   const showModalHandler = (event) => {
+      // 如果是 children 內的子元素，不關閉 modal
+      if (event.target !== event.currentTarget) {
+         return
+      } else {
+         setShow(() => !isShow)
+      }
+   }
+
+   return (
+      <StyledModalBackground showModal={isShow} onClick={showModalHandler}>
+         <StyledModalContent
+            width={size.w}
+            height={size.h}
+            top={position.t}
+            bottom={position.b}
+            left={position.l}
+            right={position.r}
+         >
+            {children}
+         </StyledModalContent>
+      </StyledModalBackground>
+   )
+}
+
+export default Modal
