72dc9db33f4750b7357372c1bb938e359e266fff
southAndy <zatchbell.1206@gmail.com>
2024-02-05 22:09:30 +0800

    feat(plugins): update Swiper's version

--- src/plugins/Swiper/SwiperSlide.jsx
+++ src/plugins/Swiper/SwiperSlide.jsx
@@ -1,38 +1,59 @@
-import { Navigation, A11y } from 'swiper'
+import React, { useRef, useState } from 'react'
+// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
 
-// customer css
-import './Swipers.scss'
+// Import Swiper styles
 import 'swiper/css'
-import 'swiper/css/navigation'
+import 'swiper/css/effect-cards'
 
-export default ({ isLoading, data }) => {
+// import required modules
+import { EffectCards } from 'swiper/modules'
+
+import { sampleExhibition } from '../../assets/images'
+
+export default function App() {
    return (
-      <Swiper
-         className='swipers'
-         modules={[Navigation, A11y]}
-         spaceBetween={50}
-         slidesPerView={3}
-         navigation={!isLoading}
-         loop={true}
-      >
-         {data.map((list, index) => {
-            return (
-               <SwiperSlide key={index} className='slide'>
-                  <Link
-                     className={isLoading ? 'skeleton' : 'slide-image'}
-                     to={`/detail/${list?.id}`}
-                  >
-                     <img
-                        src={list.imageUrl || ''}
-                        className={isLoading ? 'hide' : 'slide-image'}
-                     />
-                  </Link>
-               </SwiperSlide>
-            )
-         })}
-      </Swiper>
+      <>
+         <Swiper
+            effect={'cards'}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className='flex h-[300px] w-[250px] p-5'
+         >
+            <SwiperSlide className='flex items-center justify-center text-lg bg-red-50'>
+               <div className='flex flex-col items-center'>
+                  <div className='w-[270px] h-[170px]'>
+                     <img src={sampleExhibition} alt='' />
+                  </div>
+                  <h3>未來身體——超自然雕像</h3>
+                  <p>2021.10.1-2022.1.2</p>
+               </div>
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-green-50'>
+               Slide 2
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-blue-50'>
+               Slide 3
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-yellow-100'>
+               Slide 4
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-gray-500'>
+               Slide 5
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-yellow-600'>
+               Slide 6
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-blue-600'>
+               Slide 7
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-green-600'>
+               Slide 8
+            </SwiperSlide>
+            <SwiperSlide className='flex items-center justify-center text-lg bg-red-100'>
+               Slide 9
+            </SwiperSlide>
+         </Swiper>
+      </>
    )
 }
