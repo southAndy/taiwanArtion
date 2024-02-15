ae7fe3a1a49b44415cf1b97547be8d500ec5b55c
southAndy <zatchbell.1206@gmail.com>
2024-02-16 00:01:10 +0800

    style(login): layout login page

--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -1,60 +1,90 @@
-import { useState } from 'react'
-import { loginBannerMobile, facebookIcon, lineIcon, googleIcon } from '../assets/images/index'
-import Header from '../container/Header/Header'
+import React from 'react'
+import Header from './Header/Header'
 import styled from 'styled-components'
+import { hotBg, vectorIcon, facebookIcon, lineIcon, googleIcon } from '../assets/images/index'
+import Input from '../components/Input/Input'
+import { Button } from '../components/Button'
+import { Link } from 'react-router-dom'
 
+const StyledLoginBanner = styled.section`
+   display: flex;
+   align-items: center;
+   justify-content: start;
+   gap: 90px;
+   padding: 20px;
+   background-image: url(${hotBg});
+   height: 100px;
+`
+const StyledTitle = styled.h3`
+   font-size: 18px;
+   font-weight: 700;
+`
+const StyledContent = styled.section`
+   border-radius: 16px;
+   padding: 24px;
+`
 const StyledImageBox = styled.div`
    height: 40px;
    width: 40px;
+   cursor: pointer;
 `
-const StyledLoginTitle = styled.h2`
-   color: #be8152;
-   font-size: 18px;
-   font-weight: 500;
-`
-const StyledLoginButton = styled.button`
-   border: 2px solid #a9622a;
-   border-radius: 20px;
-   padding: 16px;
-   &:hover {
-      background: #a9622a;
-      color: #fff;
-   }
-`
-//todo 加入字體
 
-const LoginPage = () => {
+const Login = () => {
    return (
       <>
          <Header />
-         <section>
-            <div>
-               <img src={loginBannerMobile} alt='' />
-            </div>
-            <div className='p-6'>
-               <StyledLoginTitle>歡迎來到早找展覽</StyledLoginTitle>
-               <p className='text-[14px] mt-1'>開始探索下一段展覽之旅吧！</p>
-               <div className='login flex flex-col gap-2 mt-10'>
-                  <StyledLoginButton>登入</StyledLoginButton>
-                  <StyledLoginButton>註冊</StyledLoginButton>
+         <StyledLoginBanner>
+            <Link to='/account' className='w-[18px] h-[10px]'>
+               <img src={vectorIcon} alt='回到上一頁箭頭' />
+            </Link>
+            <StyledTitle className='text-md'>會員登入</StyledTitle>
+         </StyledLoginBanner>
+         <StyledContent>
+            <form className='flex flex-col gap-4 mb-10'>
+               <div className='flex flex-col'>
+                  <label htmlFor='email' className='font-medium mb-2'>
+                     帳號
+                  </label>
+                  <Input size={'12px 16px'} shape={'12px'} placeholder={'4-21碼小寫英文.數字'} />
                </div>
-               <div className='third mt-4 flex flex-col justify-center items-center'>
-                  <p className='mb-4'>或者使用以下帳號登入/註冊</p>
-                  <div className='flex justify-center items-center gap-6'>
-                     <StyledImageBox>
-                        <img src={facebookIcon} alt='臉書圖示' />
-                     </StyledImageBox>
-                     <StyledImageBox>
-                        <img src={googleIcon} alt='谷歌圖示' />
-                     </StyledImageBox>
-                     <StyledImageBox>
-                        <img src={lineIcon} alt='Line圖示' />
-                     </StyledImageBox>
-                  </div>
+               <div className='flex flex-col'>
+                  <label htmlFor='password' className='mb-2 font-medium'>
+                     密碼
+                  </label>
+                  <Input
+                     size={'12px 16px'}
+                     shape={'12px'}
+                     placeholder={'6-18位數密碼,請區分大小寫'}
+                  />
                </div>
-            </div>
-         </section>
+               <Link to='/forget-password' className='text-end text-[#A9622A] cursor-pointer'>
+                  忘記密碼？
+               </Link>
+               <Button className=' mb-8' content={'登入'} />
+            </form>
+            <section className='flex flex-col items-center gap-4 '>
+               <div>
+                  <span>還不是會員？</span>
+                  <Link to='/register' className='text-[#A9622A]'>
+                     註冊帳號
+                  </Link>
+               </div>
+               <p>或者使用以下帳號登入/註冊</p>
+               <div className='flex gap-6'>
+                  <StyledImageBox>
+                     <img src={facebookIcon} alt='' />
+                  </StyledImageBox>
+                  <StyledImageBox>
+                     <img src={googleIcon} alt='' />
+                  </StyledImageBox>
+                  <StyledImageBox>
+                     <img src={lineIcon} alt='' />
+                  </StyledImageBox>
+               </div>
+            </section>
+         </StyledContent>
       </>
    )
 }
-export default LoginPage
+
+export default Login
