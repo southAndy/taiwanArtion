36324fe9a82da914b0566613ea9eacee6009b40e
southAndy <zatchbell.1206@gmail.com>
2024-02-16 00:04:02 +0800

    feat(router): modify router's name

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/home/home'
 import ErrorPage from './pages/Errors/Error'
 import DetailPage from './pages/Detail/Detail'
 import ResultPage from './pages/Result/Result'
+import AccountPage from './container/Account'
 import LoginPage from './container/Login'
 // import MapPage from './page/Map/Map'
 import './assets/scss/main.scss'
@@ -29,6 +30,23 @@ const router = createBrowserRouter([
       path: '/result/:keyword',
       element: <ResultPage />,
       errorElement: <ErrorPage />,
+   },
+   {
+      path: '/account',
+      element: <AccountPage />,
+      errorElement: <ErrorPage />,
+      // children: [
+      //    {
+      //       path: '/account/login',
+      //       element: <LoginPage />,
+      //       errorElement: <ErrorPage />,
+      //    },
+      //    // {
+      //    //    path: '/register',
+      //    //    element: <LoginPage />,
+      //    //    errorElement: <ErrorPage />,
+      //    // },
+      // ],
    },
    {
       path: '/login',
