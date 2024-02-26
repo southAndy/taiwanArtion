f8fd39cc17b679ef28acc11d968b31e383317ebd
southAndy <zatchbell.1206@gmail.com>
2024-02-26 16:13:21 +0800

    style(all): add exhibition detail page's layout

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -6,24 +6,26 @@ const StyledInput = styled.input.attrs((props) => {
 })`
    width: 100%;
    border-radius: ${(props) => props.shape ?? '16px'};
-   border: ${(props) => (props.isError ? '1px solid #D31C1C' : '1px solid #e0e0e0')};
+   border: ${(props) => (props.formState === 'error' ? '1px solid #D31C1C' : '1px solid #e0e0e0')};
    padding: ${(props) => props.size ?? '16px'};
    font-size: 14px;
 `
 
-const Input = ({ children, placeholder, value, size, shape, setValue, isError }) => {
+const Input = ({ children, placeholder, value, size, shape, setValue, isError, formState }) => {
    const handleChange = (e) => {
       console.log(e.target.value)
       setValue(() => e.target.value)
    }
    return (
       <StyledInput
+         onChange={handleChange}
          size={size}
          shape={shape}
          isError={isError}
          placeholder={placeholder}
+         formState={formState}
          value={value}
-         onChange={handleChange}
+         disabled={formState === 'loading' || formState === 'disabled'}
       />
    )
 }
--- src/container/Register/SecondStep.jsx
+++ src/container/Register/SecondStep.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../../components/Input/Input'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import { registerInfoIcon, uncheckIcon } from '../../assets/images'
 const secondStep = () => {
    const ruleList = [
@@ -9,21 +12,52 @@ const secondStep = () => {
       '8-16位英.數字',
       '僅能使用英文.數字或特殊標點符號',
    ]
+   const schema = yup.object().shape({
+      account: yup.string().required(),
+      password: yup.string().required(),
+   })
+   const {
+      register,
+      handleSubmit,
+      formState: { errors },
+   } = useForm({
+      resolver: yupResolver(schema),
+   })
    return (
       <>
-         <form className='flex flex-col gap-4 mb-10'>
+         <form
+            onSubmit={handleSubmit((data) => {
+               console.log(data)
+            })}
+            className='flex flex-col gap-4 mb-10'
+         >
             <div className='flex flex-col'>
                <label htmlFor='email' className='font-medium mb-2'>
                   帳號
                </label>
-               <Input size={'12px 16px'} shape={'12px'} placeholder={'4-21碼小寫英文.數字'} />
+               <Input
+                  {...register('account', { required: '帳號必填' })}
+                  size={'12px 16px'}
+                  shape={'12px'}
+                  placeholder={'4-21碼小寫英文.數字'}
+               />
+               <p className='text-red-500'>{errors?.account?.message}</p>
             </div>
             <div className='flex flex-col'>
                <label htmlFor='password' className='mb-2 font-medium'>
                   密碼
                </label>
-               <Input size={'12px 16px'} shape={'12px'} placeholder={'6-18位數密碼,請區分大小寫'} />
+               <Input
+                  {...register('password', { required: '76' })}
+                  size={'12px 16px'}
+                  shape={'12px'}
+                  placeholder={'6-18位數密碼,請區分大小寫'}
+                  formState={'error'}
+               />
+               <p className='text-red-500'>{errors?.password?.message}</p>
             </div>
+
+            <button type='submit'>click</button>
          </form>
          <section>
             <h3 className='flex gap-2 items-center font-bold mb-2'>
