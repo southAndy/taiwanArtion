b40c57c158dc25f886589d9f612f08b73b505167
southAndy <zatchbell.1206@gmail.com>
2024-02-29 21:24:04 +0800

    refactor(router): module router logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './pages/home/home'
-import ErrorPage from './pages/Errors/Error'
-import DetailPage from './pages/Detail/Detail'
-import ResultPage from './pages/Result/Result'
-import AccountPage from './container/Account'
-import LoginPage from './container/Login'
-import Register from './pages/Register/Register'
-import Backstage from './pages/Backstage/Backstage'
-// import MapPage from './page/Map/Map'
+import router from './router/index'
+import { RouterProvider } from 'react-router-dom'
 import './assets/scss/main.scss'
 import Header from './container/Header/Header'
+
 // import app from '../firebase.config'
 
 import { Provider } from 'react-redux'
 //redux instance
 import store from './store'
 
-const router = createBrowserRouter([
-   {
-      path: '/',
-      element: <HomePage />,
-   },
-   {
-      path: '/detail/:id',
-      element: <DetailPage />,
-      errorElement: <ErrorPage />,
-   },
-   {
-      path: '/result/:keyword',
-      element: <ResultPage />,
-      errorElement: <ErrorPage />,
-   },
-   {
-      path: '/account',
-      element: <AccountPage />,
-      errorElement: <ErrorPage />,
-   },
-   {
-      path: '/login',
-      element: <LoginPage />,
-      errorElement: <ErrorPage />,
-   },
-   {
-      path: '/register',
-      element: <Register />,
-      errorElement: <ErrorPage />,
-   },
-   {
-      path: '/backstage/',
-      element: <Backstage />,
-      errorElement: <ErrorPage />,
-   },
-])
 const rootElement = document.getElementById('root')
 if (rootElement) {
    ReactDOM.createRoot(rootElement).render(
