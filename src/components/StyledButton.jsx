7fb3e5cc1b936175cfd9f458d2b6db9167c75081
southAndy <zatchbell.1206@gmail.com>
2024-03-02 20:56:49 +0800

    style(component): add common style component

---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,16 @@
+const StyledButton = styled.button.attrs({
+   type: 'button',
+})`
+   color: ${(props) => (props.disabled ? '#3333' : ' #eeeeee')};
+   background-color: ${(props) => (props.disabled ? '#EEEEEE' : ' #be875c')};
+   border-radius: 12px;
+   width: 100%;
+   border: ${(props) => (props.border ? props.border : 'none')};
+   padding: 8px 24px;
+   margin: ${(props) => props.margin ?? '0'};
+   &:disabled {
+      background-color: #eeeeee;
+      color: #3333;
+   }
+`
+export default StyledButton
