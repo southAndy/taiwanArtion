b0ec8695fd412dfa9c43766b1cee9c1b86cf09f4
southAndy <zatchbell.1206@gmail.com>
2024-03-03 18:01:08 +0800

    feat(register): add password tips

--- src/container/Register/SecondStep.jsx
+++ src/container/Register/SecondStep.jsx
@@ -3,23 +3,72 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Input from '../../components/Input/Input'
 import StyledInput from '../../components/StyledInput'
-import { useForm } from 'react-hook-form'
+import { set, useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-import { registerInfoIcon, uncheckIcon } from '../../assets/images'
+import { registerInfoIcon, uncheckIcon, warnIcon } from '../../assets/images'
 import axios from 'axios'
+
 const secondStep = () => {
    const [account, setAccount] = useState('')
    const [password, setPassword] = useState('')
+   const [score, setScore] = useState(0)
+   const [matchTips, setMatchTips] = useState([false, false, false, false])
+
    const ruleList = [
-      '至少一個小寫字母',
-      '至少一個大寫字母',
-      '8-16位英.數字',
-      '僅能使用英文.數字或特殊標點符號',
+      '至少包含一個小寫字母',
+      '至少包含一個大寫字母',
+      '長度為 8-16位英.數字',
+      '加入至少一個特殊標點符號',
    ]
    const schema = yup.object().shape({
       account: yup.string().required(),
-      password: yup.string().required(),
+      password: yup
+         .string()
+         .required()
+         .test('test', '不符合密碼強度', () => {
+            setScore(0)
+            //密碼至少包含一個小寫字母
+            if (password.match(/[a-z]/)) {
+               setScore((n) => n + 25)
+               //修改 matchTips index0 的值
+               setMatchTips((prev) => {
+                  const newTips = [...prev]
+                  newTips[0] = true
+                  return newTips
+               })
+            }
+            //密碼至少包含一個大寫字母
+            if (password.match(/[A-Z]/)) {
+               setScore((n) => n + 25)
+               //修改 matchTips index1 的值
+               // setMatchTips((prev) => {
+               //    const newTips = [...prev]
+               //    newTips[1] = true
+               //    return newTips
+               // })
+            }
+            if (password.length >= 8 && password.length <= 16) {
+               setScore((n) => n + 25)
+               //修改 matchTips index2 的值
+               // setMatchTips((prev) => {
+               //    const newTips = [...prev]
+               //    newTips[2] = true
+               //    return newTips
+               // })
+            }
+            //加入至少一個特殊標點符號
+            if (password.match(/[\W_]/)) {
+               setScore((n) => n + 25)
+               //修改 matchTips index3 的值
+               // setMatchTips((prev) => {
+               //    const newTips = [...prev]
+               //    newTips[3] = true
+               //    return newTips
+               // })
+            }
+            return score >= 100
+         }),
    })
    const {
       register,
@@ -27,34 +76,14 @@ const secondStep = () => {
       formState: { errors },
    } = useForm({
       resolver: yupResolver(schema),
+      mode: 'all',
    })
-   useEffect(() => {
-      console.log(account)
-      // const validateAccount = async () => {
-      //    try {
-      //       let res = await axios.post(
-      //          'https://zhao-zhao-zhan-lan-hou-duan-ce-shi-fu-wu.onrender.com/auth/account',
-      //          {
-      //             account: account,
-      //          },
-      //       )
-      //       console.log(res)
-      //    } catch (e) {
-      //       console.log(e)
-      //    }
-      // }
-      // validateAccount()
-   }, [account])
+
    return (
       <>
-         <form
-            onSubmit={handleSubmit((data) => {
-               console.log(data)
-            })}
-            className='flex flex-col gap-4 mb-10'
-         >
+         <form onSubmit={handleSubmit((data) => {})} className='flex flex-col gap-4 mb-10'>
             <div className='flex flex-col'>
-               <label htmlFor='email' className='font-medium mb-2'>
+               <label htmlFor='email' className='font-medium mb-2 text-[#453434]'>
                   帳號
                </label>
                <StyledInput
@@ -64,10 +93,19 @@ const secondStep = () => {
                   placeholder={'4-21碼小寫英文.數字'}
                   onChange={(e) => setAccount(e.target.value)}
                />
-               <p className='text-red-500'>{errors?.account?.message}</p>
+               {errors.account ? (
+                  <div className='flex gap-1 mt-2'>
+                     <div className='w-[20px] h-[20px]'>
+                        <img src={warnIcon} alt='' />
+                     </div>
+                     <span className='text-[#D31C1C]'>{errors.account?.message}</span>
+                  </div>
+               ) : (
+                  ''
+               )}
             </div>
             <div className='flex flex-col'>
-               <label htmlFor='password' className='mb-2 font-medium'>
+               <label htmlFor='password' className='mb-2 font-medium text-[#453434]'>
                   密碼
                </label>
                <StyledInput
@@ -76,13 +114,22 @@ const secondStep = () => {
                   size={'12px 16px'}
                   shape={'12px'}
                   placeholder={'6-18位數密碼,請區分大小寫'}
+                  onChange={(e) => setPassword(e.target.value)}
                />
-               <p className='text-red-500'>{errors?.password?.message}</p>
+               {errors.password ? (
+                  <div className='flex gap-1 mt-2'>
+                     <div className='w-[20px] h-[20px]'>
+                        <img src={warnIcon} alt='' />
+                     </div>
+                     <span className='text-[#D31C1C]'>{errors.password?.message}</span>
+                  </div>
+               ) : (
+                  ''
+               )}
             </div>
-            <button type='submit'>click</button>
          </form>
          <section>
-            <h3 className='flex gap-2 items-center font-bold mb-2'>
+            <h3 className='flex gap-2 items-center font-bold mb-2 text-[#453434]'>
                <div className='w-[20px] h-[20px]'>
                   <img src={registerInfoIcon} alt='' />
                </div>
@@ -96,7 +143,10 @@ const secondStep = () => {
                   {text}
                </div>
             ))}
-            {/* <div className='mt-4'>密碼強度</div> */}
+            <div>
+               <div className='mt-4 text-[#453434]'>密碼強度</div>
+               <progress id='score' className='w-[100%]' value={score} max='100'></progress>
+            </div>
          </section>
       </>
    )
