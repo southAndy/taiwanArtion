a2d499f04aecb40436ab9216cea5c31f5f46c7c4
southAndy <zatchbell.1206@gmail.com>
2024-03-06 20:56:50 +0800

    fix(home): modify structure of data

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -175,7 +175,7 @@ const HomePage = () => {
                'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6',
             )
             // 把兩個資料合併
-            const mergeData = [...response.data, ...openResponse.data]
+            const mergeData = [...response.data, ...openResponse]
             console.log('合併結果', mergeData)
 
             setList(() => openResponse.data)
