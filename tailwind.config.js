887b08d05f2dda197796de26f4eab938f9c573d3
southAndy <zatchbell.1206@gmail.com>
2024-08-11 15:33:13 +0800

    feat(home): remove styled-compoent codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,jsx,tsx}"],
+  content: ["./src/**/*.{html,jsx,tsx}"], // detect what files to look at
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hot': "url('/src/assets/images/hot-bg.png')",
+      },
+      // website's system color
+      colors:{
+
+      }
+    },
   },
   plugins: [],
 };
