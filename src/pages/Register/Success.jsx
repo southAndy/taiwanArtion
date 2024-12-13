73174b5a665700cdd7504929fa89dfe3eec6ac96
southAndy <zatchbell.1206@gmail.com>
2024-12-13 17:34:41 +0800

    feat(register):rewrite register successful by styled

--- src/pages/Register/Success.jsx
+++ src/pages/Register/Success.jsx
@@ -5,6 +5,29 @@ import { Link } from 'react-router-dom'
 import { successBg, sucessIcon } from '../../assets/images'
 import styled from 'styled-components'
 
+const Success = () => {
+   useEffect(() => {
+      // cookie 新增登入的狀態
+      document.cookie = 'isLogin=true'
+   }, [])
+   return (
+      <>
+         <Header />
+         <StyledBanner className=' bg-slate-200'>
+            <div className='h-[200px] w-[390px]'>
+               <img src={sucessIcon} alt='' />
+            </div>
+            <section className='content flex gap-2 flex-col justify-center items-center '>
+               <h1 className='text-[#A9622A] font-bold text-xl'>註冊成功</h1>
+               <p className='text-sm mb-10'>恭喜您,已完成註冊！</p>
+               <StyledPersonalLink to={'/backstage'}>開始填寫個人檔案</StyledPersonalLink>
+               <StyledHomeLink to={'/'}>回到首頁</StyledHomeLink>
+            </section>
+         </StyledBanner>
+      </>
+   )
+}
+
 const StyledPersonalLink = styled(Link)`
    display: block;
    color: #fff;
@@ -34,31 +57,27 @@ const StyledBanner = styled.section`
    align-items: center;
    height: 100vh;
    background-image: url(${successBg});
-   background-size: cover;
+   background-size: contain;
    background-position: center;
    background-color: #fdfaef;
+
+   .content {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      h1 {
+         color: #a9622a;
+         font-weight: 600;
+         font-size: 18px;
+         text-align: center;
+         margin: unset;
+      }
+      p {
+         font-size: 14px;
+         margin-bottom: 32px;
+         text-align: center;
+      }
+   }
 `
 
-const Success = () => {
-   useEffect(() => {
-      // cookie 新增登入的狀態
-      document.cookie = 'isLogin=true'
-   }, [])
-   return (
-      <>
-         <Header />
-         <StyledBanner className=' bg-slate-200'>
-            <div className='h-[200px] w-[390px]'>
-               <img src={sucessIcon} alt='' />
-            </div>
-            <section className='flex gap-2 flex-col justify-center items-center '>
-               <h1 className='text-[#A9622A] font-bold text-xl'>註冊成功</h1>
-               <p className='text-sm mb-10'>恭喜您,已完成註冊！</p>
-               <StyledPersonalLink to={'/backstage'}>開始填寫個人檔案</StyledPersonalLink>
-               <StyledHomeLink to={'/'}>回到首頁</StyledHomeLink>
-            </section>
-         </StyledBanner>
-      </>
-   )
-}
 export default Success
