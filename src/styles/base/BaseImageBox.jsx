a1190d96ee8e9824241486601466ba979943fa20
southAndy <zatchbell.1206@gmail.com>
2024-12-13 19:03:44 +0800

    feat(styled): modify common component

--- src/styles/base/BaseImageBox.jsx
+++ src/styles/base/BaseImageBox.jsx
@@ -3,4 +3,5 @@ import styled from 'styled-components'
 export default styled.div`
    width: ${(props) => props.width};
    height: ${(props) => props.height};
+   object-fit: ${(props) => (props.height ? props.types : 'contain')};
 `
