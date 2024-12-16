334c95048f3dc42fd30ead39b13c7cab46e099fb
southAndy <zatchbell.1206@gmail.com>
2024-12-16 13:41:19 +0800

    feat(detail): fetch api

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import { setMemberInterests } from '../../store/memberSlice'
 import Header from '../../container/Header/Header'
 import Button from '../../components/Button'
@@ -15,33 +15,51 @@ import {
    likeIcon,
 } from '../../assets/images'
 import styled from '@emotion/styled'
+import axios from 'axios'
 
 export default function DetailPage() {
    let [exhibition, setExhibition] = useState([])
    const params = useParams()
    const navigate = useNavigate()
    const [currentOption, setCurrentOption] = useState(0)
-   // 是否收藏此展覽
-   const [isStore, setIsStore] = useState(false)
-   const dispatch = useDispatch()
-   const isLogin = useSelector((state) => state.member.isLogin)
-   const interestList = useSelector((state) => state.member.interests)
+   const [isStore, setIsStore] = useState(false) // 聯動收藏愛心 icon
+   // const dispatch = useDispatch()
+   // const isLogin = useSelector((state) => state.member.isLogin)
+   // const interestList = useSelector((state) => state.member.interests)
    const currentData = useMemo(() => {
-      return exhibition.filter((item) => item.UID === params.dataID && item.imageUrl !== '')
+      return exhibition.filter((item) => item.UID === params.id)
    }, [exhibition])
 
-   const storeExhibition = () => {
-      //先判斷是否登入
-      if (isLogin) {
-         // 如果登入，可以將此展覽加入收藏
-         setIsStore(true)
-         //加入會員的收藏清單
-         dispatch(setMemberInterests(['ID12354E']))
-      } else {
-         // 如果沒登入，跳轉到登入頁面
-         navigate('/login')
+   async function getExhibition() {
+      try {
+         const res = await axios.get(
+            'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6',
+         )
+         const data = res.data
+         setExhibition((n) => (n = data))
+      } catch (e) {
+         console.log(e)
       }
    }
+
+   useEffect(() => {
+      //取得 params 的景點 ID
+      const id = params.id
+      getExhibition()
+   }, [])
+
+   // const storeExhibition = () => {
+   //    //先判斷是否登入
+   //    if (isLogin) {
+   //       // 如果登入，可以將此展覽加入收藏
+   //       setIsStore(true)
+   //       //加入會員的收藏清單
+   //       dispatch(setMemberInterests(['ID12354E']))
+   //    } else {
+   //       // 如果沒登入，跳轉到登入頁面
+   //       navigate('/login')
+   //    }
+   // }
    return (
       <>
          <Header />
@@ -53,12 +71,10 @@ export default function DetailPage() {
                   </BaseImageBox>
                   <Link to={'/'}>返回首頁</Link>
                </div>
-               <div className='h-[285px] mb-6 rounded-xl'>
-                  <img className='rounded-xl' src={sampleResult} />
-               </div>
-               <h3 className='text-[24px] font-bold'>
-                  {'會動的文藝復興 Friendship Blooming in Music'}
-               </h3>
+               <BaseImageBox width={'327px'} height={'245px'}>
+                  <img className='rounded-xl' src={currentData[0]?.imageUrl} />
+               </BaseImageBox>
+               <h3 className='text-[24px] font-bold'>{currentData[0]?.title}</h3>
             </DetailBanner>
             <section className='menu'>
                <DetailOption isActive={true} href='#overview'>
@@ -82,51 +98,40 @@ export default function DetailPage() {
                <div className='info'>
                   <div className='info-option'>
                      <div>展覽日期</div>
-                     <div>2022.12.01(一）- 2022.12.01(一）</div>
+                     <div>{`${currentData[0]?.startDate} - ${currentData[0]?.endDate}`}</div>
                   </div>
                   <div className='info-option'>
                      <div>營業時間</div>
-                     <div>9:00a.m - 10:00p.m</div>
+                     <div>{'9:00a.m'}</div>
                   </div>
                   <div className='info-option'>
                      <div>主辦單位</div>
-                     <div>Queeny女人迷你俱樂部</div>
+                     <div>{currentData[0]?.showUnit}</div>
                   </div>
 
                   <div className='info-option'>
                      <div>展覽官網</div>
                      <a href='https://womany.net/terms' className='flex text-blue-300'>
-                        Queeny女人迷你俱樂部
+                        {'-'}
                      </a>
                   </div>
                   <div className='info-option'>
                      <div>展覽電話</div>
-                     <div>(02)-23530506</div>
+                     <div>{'-'}</div>
                   </div>
                </div>
             </StyledInfo>
             <StyledInfo>
                <StyledInfoTitle id='introduce'>展覽介紹</StyledInfoTitle>
-               <p>
-                  自然寫作中的一種特殊形式，是「動物小說」。人將自己放到動物的環境中，探索動物與自然的關係，進而從中獲取如何與自然共處的教訓或智慧。讀者當然知道作者不可能真正化身為動物，然而卻又被刺激出了最高度的好奇心與跨物種的同情心。
-                  有一個相當長的時代，從美國開始而影響全世界，將「動物小說」視為小孩，至少是小男孩成長必備的讀物，強調「動物小說」所能提供的特殊情感體驗。『野性的呼喚』和『鹿苑長春』就是那個時代中出現最具有代表性的經典作品...自然寫作中的一種特殊形式，是「動物小說」。人將自己放到動物的環境中，探索動物與自然的關係，進而從中獲取如何與自然共處的教訓或智慧。讀者當然知道作者不可能真正化身為動物，然而卻又被刺激出了最高度的好奇心與跨物種的同情心。
-                  有一個相當長的時代，從美國開始而影響全世界，將「動物小說」視為小孩，至少是小男孩成長必備的讀物，強調「動物小說」所能提供的特殊情感體驗。『野性的呼喚』和『鹿苑長春』就是那個時代中出現最具有代表性的經典作品...自然寫作中的一種特殊形式，是「動物小說」。
-               </p>
+               <p>{currentData[0]?.descriptionFilterHtml}</p>
             </StyledInfo>
             <StyledInfo>
                <StyledInfoTitle id='price'>展覽票價</StyledInfoTitle>
-               <p>
-                  全票 ＄199 備註：11/25-12/1優惠活動正式開跑,凡購買早鳥優惠送一顆大蘋果。
-                  學生票＄199 備註：11/25-12/1優惠活動正式開跑,凡購買早鳥優惠送一顆大蘋果。
-                  團體票＄199 備註：11/25-12/1優惠活動正式開跑,凡購買早鳥優惠送一顆大蘋果。
-                  愛心票＄199 備註：11/25-12/1優惠活動正式開跑,凡購買早鳥優惠送一顆大蘋果。 免票入場
-                  須出示相關證件 年齡未滿五歲之兒童
-                  (須由一位大人持票陪同入場)，未帶證件以身高110公分作判定。
-               </p>
+               <p>{currentData[0]?.webSales ?? '-'}</p>
             </StyledInfo>
+            {/* todo  */}
             <StyledInfo>
-               <StyledInfoTitle id='place'>展覽地點</StyledInfoTitle>
-               <div>place</div>
+               <StyledInfoTitle id='place'></StyledInfoTitle>
             </StyledInfo>
             <StyledInfo>
                <div className='menu flex gap-3 whitespace-nowrap'>
@@ -190,7 +195,7 @@ export default function DetailPage() {
             </StyledInfo>
          </DetailContainer>
          <StyledToolBar>
-            <div className='option' onClick={storeExhibition}>
+            <div className='option'>
                <BaseImageBox width={'24px'} height={'24px'}>
                   <img src={loveIcon} alt='收藏此展覽按鈕' />
                </BaseImageBox>
