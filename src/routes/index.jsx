96bdd93d460514390117a4c5f4772db50bc1df0b
southAndy <zatchbell.1206@gmail.com>
2024-12-16 16:28:15 +0800

    feat(router):add reset password's route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -41,8 +41,8 @@ const router = createBrowserRouter([
       element: <Register />,
    },
    // {
-   //    path: '/success',
-   //    element: <Success />,
+   //    path: '/reset',
+   //    element: <Reset />,
    // },
    {
       path: '/backstage/',
