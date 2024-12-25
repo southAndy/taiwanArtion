0196daa74fe4cfeba95c980fe743522664a1d91d
southAndy <zatchbell.1206@gmail.com>
2024-12-25 14:38:55 +0800

    feat(backstage): add the feature of  switch user photo

--- src/assets/images/backstage/index.js
+++ src/assets/images/backstage/index.js
@@ -1,3 +1,25 @@
 import selectPhotoIcon from '../camera.png'
+import userIcon0 from '../backstage/user/user0.png'
+import userIcon1 from '../backstage/user/user1.png'
+import userIcon2 from '../backstage/user/user2.png'
+import userIcon3 from '../backstage/user/user3.png'
+import userIcon4 from '../backstage/user/user4.png'
+import userIcon5 from '../backstage/user/user5.png'
+import userIcon6 from '../backstage/user/user6.png'
+import userIcon7 from '../backstage/user/user7.png'
+import userIcon8 from '../backstage/user/user8.png'
+import userIcon9 from '../backstage/user/user9.png'
 
-export { selectPhotoIcon }
+export {
+   selectPhotoIcon,
+   userIcon0,
+   userIcon1,
+   userIcon2,
+   userIcon3,
+   userIcon4,
+   userIcon5,
+   userIcon6,
+   userIcon7,
+   userIcon8,
+   userIcon9,
+}
