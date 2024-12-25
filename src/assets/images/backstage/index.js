3a49927f06c2f1e8c9a49bbfd207fd4e8fcd473f
southAndy <zatchbell.1206@gmail.com>
2024-12-25 17:36:50 +0800

    feat(backstage):add the layout of unsaved exhibition screen

--- src/assets/images/backstage/index.js
+++ src/assets/images/backstage/index.js
@@ -9,6 +9,7 @@ import userIcon6 from '../backstage/user/user6.png'
 import userIcon7 from '../backstage/user/user7.png'
 import userIcon8 from '../backstage/user/user8.png'
 import userIcon9 from '../backstage/user/user9.png'
+import notFoundBg from '../notfind-bg.png'
 
 export {
    selectPhotoIcon,
@@ -22,4 +23,5 @@ export {
    userIcon7,
    userIcon8,
    userIcon9,
+   notFoundBg,
 }
