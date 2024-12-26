92bde179871b2454d1d6ccc2d4e4efeef2064d44
southAndy <zatchbell.1206@gmail.com>
2024-12-26 17:02:09 +0800

    feat(backstage):modify profile menu's layout

--- src/pages/Backstage/ProfileMenu.jsx
+++ src/pages/Backstage/ProfileMenu.jsx
@@ -23,39 +23,39 @@ const ProfileMenu = () => {
             <button type='button' className='edit' onClick={() => setIsEdit((n) => !n)}>
                編輯
             </button>
-            <label htmlFor='name'>
+            <label htmlFor='name' className='name'>
                <p>姓名</p>
                <p className={`${isEdit ? 'none' : 'show'}`}>Andy</p>
                <input className={`${isEdit ? 'show' : 'none'}`} type='text' />
             </label>
-            <label htmlFor='email'>
+            <label htmlFor='account' className='account'>
                <p>使用者帳號</p>
                <p>1222test</p>
             </label>
-            <label htmlFor='password'>
+            <label htmlFor='emaul' className='email'>
                <p>註冊信箱</p>
                <p>zatchbell.1206@gmail.com</p>
                <span>變更電子信箱</span>
             </label>
-            <label htmlFor='password'>
+            <label htmlFor='birthday' className='birthday'>
                <p>生日</p>
                <p className={`${isEdit ? 'none' : 'show'}`}>84/12/06</p>
                <select name='' id='' className={`${isEdit ? 'show' : 'none'}`}></select>
             </label>
-            <label htmlFor=''>
+            <label htmlFor='gender' className='gender'>
                <p>性別</p>
                <p className={`${isEdit ? 'none' : 'show'}`}>男性</p>
                <select name='' id='' className={`${isEdit ? 'show' : 'none'}`}></select>
             </label>
-            <label htmlFor=''>
+            <label htmlFor='place' className='place'>
                <p>居住地點</p>
                <p className={`${isEdit ? 'none' : 'show'}`}>高雄市</p>
                <input className={`${isEdit ? 'show' : 'none'}`} type='text' />
             </label>
             {isEdit ? (
-               <div>
-                  <button>取消</button>
-                  <button>儲存</button>
+               <div className='option'>
+                  <button className='cancel'>取消</button>
+                  <button className='save'>儲存</button>
                </div>
             ) : null}
          </StyledFormBox>
@@ -70,6 +70,7 @@ const StyledProfileMenu = styled.main`
    flex-direction: column;
    align-items: flex-start;
    justify-content: center;
+   position: relative;
    width: 100%;
    height: 100%;
    box-sizing: border-box;
@@ -78,11 +79,13 @@ const StyledProfileMenu = styled.main`
       .title {
          display: flex;
          flex-direction: column;
+         gap: 8px;
          align-items: flex-start;
          margin-bottom: 24px;
          h1 {
             font-size: 24px;
             color: #7b4d29;
+            margin: 0;
          }
          &-icon {
             display: flex;
@@ -96,7 +99,6 @@ const StyledFormBox = styled.form.attrs({})`
    display: flex;
    flex-direction: column;
    gap: 16px;
-   position: relative;
    width: 100%;
    box-sizing: border-box;
    font-family: Noto Sans TC;
@@ -115,9 +117,75 @@ const StyledFormBox = styled.form.attrs({})`
    .edit {
       position: absolute;
       right: 0;
+      top: 0;
    }
 
    @media (min-width: ${breakpoint.tablet}px) {
       font-size: 16px;
+
+      .name {
+         input {
+            width: 100%;
+            height: 40px;
+            padding: 0 16px;
+            border: 1px solid #929292;
+            border-radius: 4px;
+         }
+      }
+      .email {
+         span {
+            color: #be8152;
+            cursor: pointer;
+         }
+      }
+      .birthday {
+         select {
+            width: 100%;
+            height: 40px;
+            padding: 0 16px;
+            border: 1px solid #929292;
+            border-radius: 4px;
+         }
+      }
+      .gender {
+         select {
+            width: 100%;
+            height: 40px;
+            padding: 0 16px;
+            border: 1px solid #929292;
+            border-radius: 4px;
+         }
+      }
+      .place {
+         input {
+            width: 100%;
+            height: 40px;
+            padding: 0 16px;
+            border: 1px solid #929292;
+            border-radius: 4px;
+         }
+      }
+      .option {
+         display: flex;
+         justify-content: center;
+         gap: 8px;
+         .cancel {
+            background: #eeeeee;
+            width: 64px;
+            border-radius: 10px;
+            border: none;
+            padding: 10px;
+            cursor: pointer;
+         }
+         .save {
+            background: #a9622a;
+            width: 144px;
+            color: white;
+            border-radius: 10px;
+            border: none;
+            padding: 10px;
+            cursor: pointer;
+         }
+      }
    }
 `
