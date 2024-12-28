91bff42230fc363c8ce44f16661583a59b49e643
southAndy <zatchbell.1206@gmail.com>
2024-12-28 15:31:36 +0800

    feat(map):create google map's instance

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,8 +1,36 @@
 import React, { useEffect, useState } from 'react'
+import Header from '../../container/Header/Header'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
-export default Map = () => {
-   //初始化地圖
-   useEffect(() => {}, [])
-
-   return <></>
+const containerStyle = {
+   width: '100%',
+   height: '100vh',
+   marginTop: '-56px', //抵銷Header高度
 }
+
+const MapPage = () => {
+   // 取得使用者位置
+   const [userLocation, setUserLocation] = useState(null)
+   useEffect(() => {
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition((position) => {
+            setUserLocation({
+               lat: position.coords.latitude,
+               lng: position.coords.longitude,
+            })
+         })
+      }
+   }, [])
+   return (
+      <>
+         <Header />
+         <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
+            <GoogleMap mapContainerStyle={containerStyle} center={userLocation} zoom={15}>
+               <Marker position={userLocation} />
+            </GoogleMap>
+         </LoadScript>
+      </>
+   )
+}
+
+export default MapPage
