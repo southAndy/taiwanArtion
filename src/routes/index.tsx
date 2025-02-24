dbd1cbaed346b8dec079c647133c14e6d8d39053
southAndy <zatchbell.1206@gmail.com>
2025-02-24 15:45:12 +0800

    refactor(route):Rewrite route's file by typescript

---
+++ src/routes/index.tsx
@@ -0,0 +1,72 @@
+import { createBrowserRouter, RouteObject } from 'react-router-dom'
+import HomePage from '../pages/Home/Home'
+import ErrorPage from '../pages/Errors/Error'
+// import DetailPage from '../pages/Detail/Detail'
+// import ResultPage from '../pages/Result/Result'
+// import AccountPage from '../container/Account'
+// import LoginPage from '../container/Login'
+// import Register from '../pages/Register/Register'
+// import Backstage from '../pages/Backstage/Backstage'
+// import Success from '../pages/Register/Success'
+// import Login from '../container/Login'
+// import MapPage from '../pages/Map/Map'
+// import ProtectedRoute from './ProtectedRoute'
+// import handleLogin from './authLogin'
+
+const routes: RouteObject[] = [
+   {
+      path: '/',
+      element: <HomePage />,
+      errorElement: <ErrorPage />,
+      children: [],
+   },
+   // {
+   //    path: '/map',
+   //    element: <MapPage />,
+   // },
+   // {
+   //    path: '/detail/:id',
+   //    element: <DetailPage />,
+   // },
+   // {
+   //    path: '/result/',
+   //    element: <ResultPage />,
+   // },
+   // {
+   //    path: '/account',
+   //    element: (
+   //       <ProtectedRoute>
+   //          <AccountPage />
+   //       </ProtectedRoute>
+   //    ),
+   //    // 新增檢查權限
+   // },
+   // {
+   //    path: '/login',
+   //    element: (
+   //       <ProtectedRoute>
+   //          <LoginPage />
+   //       </ProtectedRoute>
+   //    ),
+   // },
+   // {
+   //    path: '/register',
+   //    element: (
+   //       <ProtectedRoute>
+   //          <Register />
+   //       </ProtectedRoute>
+   //    ),
+   // },
+   // // {
+   // //    path: '/reset',
+   // //    element: <Reset />,
+   // // },
+   // {
+   //    path: '/backstage/',
+   //    element: <Backstage />,
+   //    loader: handleLogin,
+   // },
+]
+const router = createBrowserRouter(routes)
+
+export default router
