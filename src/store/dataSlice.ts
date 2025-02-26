4b6c0a82d4fcf924ce10517341706ae70392460a
southAndy <zatchbell.1206@gmail.com>
2025-02-26 15:56:50 +0800

    refactor(redux):Add typescript's type

--- src/store/dataSlice.ts
+++ src/store/dataSlice.ts
@@ -7,8 +7,6 @@ const dataSlice = createSlice({
       exhibitionData: [], //展覽資料
    },
    reducers: {},
-   //todo 根據不同的action type做不同的處理
-   actions: {},
 })
-export const { showModal, hideModal } = dataSlice.actions
+
 export default dataSlice.reducer
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-// 各種 reducer
 import commonSlice from './commonSlice'
 import memberSlice from './memberSlice'
 import dataSlice from './dataSlice'
+
 const store = configureStore({
    reducer: {
       common: commonSlice,
@@ -10,5 +10,10 @@ const store = configureStore({
       data: dataSlice,
    },
 })
+
+// 透過 store.getState 回傳值來反推導狀態型別，而不是直接寫死
+export type RootState = ReturnType<typeof store.getState>
+// 透過 store.dispatch 來推導型別，因為 dispatch 可能有多個參數（根據 執行的是否是 thunk action）
+export type AppDispatch = typeof store.dispatch
 
 export default store
