c605374b4a9f81918ca42c79d56d2b12d2ff32b9
southAndy <zatchbell.1206@gmail.com>
2025-05-28 20:41:03 +0800

    test: Modify login route to dynamic route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,20 @@
+import React, { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '../pages/home/home'
 import ErrorPage from '../pages/Errors/Error'
 import DetailPage from '../pages/Detail/Detail'
 import ResultPage from '../pages/Result/Result'
 import AccountPage from '../container/Account'
-import LoginPage from '../container/Login'
+// import LoginPage from '../container/Login'
 import Register from '../pages/Register/Register'
 import Backstage from '../pages/Backstage/Backstage'
 // import Success from '../pages/Register/Success'
-import Login from '../container/Login'
+// import Login from '../container/Login'
 import MapPage from '../pages/Map/Map'
 import ProtectedRoute from './ProtectedRoute'
 import handleLogin from './authLogin'
+
+const LoginPage = lazy(() => import('../container/Login'))
 
 const router = createBrowserRouter([
    {
@@ -45,7 +48,9 @@ const router = createBrowserRouter([
       path: '/login',
       element: (
          <ProtectedRoute>
-            <LoginPage />
+            <Suspense fallback={<div>Loading...</div>}>
+               <LoginPage />
+            </Suspense>
          </ProtectedRoute>
       ),
    },
