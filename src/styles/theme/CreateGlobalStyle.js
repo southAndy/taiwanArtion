5f47674c6462acc348b3de2d345dbde5d5fee52e
southAndy <zatchbell.1206@gmail.com>
2025-06-03 15:44:10 +0800

    style:Add global a tag's styling

--- src/styles/theme/CreateGlobalStyle.js
+++ src/styles/theme/CreateGlobalStyle.js
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const CreateGlobalStyle = createGlobalStyle`
-   * {
+   body {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
+   }
+   a{
+      text-decoration: none;
+      color: #000;
    }
 `
 
