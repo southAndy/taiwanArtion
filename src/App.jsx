2d8fbd088c8d6d47e375e3195ca4410fa4de80f2
southAndy <zatchbell.1206@gmail.com>
2025-06-03 15:35:09 +0800

    style:Add global styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux'
 import store from './store'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/index'
+import CreateGlobalStyle from './styles/theme/CreateGlobalStyle'
 
 const App = () => {
    return (
       <Provider store={store}>
+         <CreateGlobalStyle />
          <RouterProvider router={router}>
             <Header />
          </RouterProvider>
---
+++ src/styles/theme/CreateGlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const CreateGlobalStyle = createGlobalStyle`
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+`
+
+export default CreateGlobalStyle
