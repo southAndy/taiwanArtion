59eb216cdbf42d19a0a2e24526038518d47a1da4
southAndy <zatchbell.1206@gmail.com>
2025-07-02 15:54:30 +0800

    feat(redux):add rule

--- src/store/commonSlice.js
+++ src/store/commonSlice.js
@@ -2,50 +2,52 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 const fetchData = createAsyncThunk('common/fetchData', async () => {
-   try {
-      const openResponse = await axios.get(
-         'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6',
-      )
-      return openResponse.data
-   } catch (e) {
-      console.log(e)
-   }
+  try {
+    const openResponse = await axios.get(
+      'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6'
+    )
+    // 且要有照片的資料
+    const data = openResponse.data.filter(item => item.imageUrl)
+    return data
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 const commonSlice = createSlice({
-   name: 'common',
-   initialState: {
-      isShowModal: false,
-      isLoading: false,
-      openData: [],
-   },
-   reducers: {
-      showModal(state) {
-         state.isShowModal = true
-      },
-      hideModal(state) {
-         state.isShowModal = false
-      },
-      showLoading(state) {
-         state.isLoading = true
-      },
-      hideLoading(state) {
-         state.isLoading = false
-      },
-   },
-   extraReducers: (builder) => {
-      builder
-         .addCase(fetchData.pending, (state) => {
-            state.isLoading = true
-         })
-         .addCase(fetchData.fulfilled, (state, action) => {
-            state.isLoading = false
-            state.openData = action.payload
-         })
-         .addCase(fetchData.rejected, (state) => {
-            state.isLoading = false
-         })
-   },
+  name: 'common',
+  initialState: {
+    isShowModal: false,
+    isLoading: false,
+    openData: [],
+  },
+  reducers: {
+    showModal(state) {
+      state.isShowModal = true
+    },
+    hideModal(state) {
+      state.isShowModal = false
+    },
+    showLoading(state) {
+      state.isLoading = true
+    },
+    hideLoading(state) {
+      state.isLoading = false
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchData.pending, state => {
+        state.isLoading = true
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.openData = action.payload
+      })
+      .addCase(fetchData.rejected, state => {
+        state.isLoading = false
+      })
+  },
 })
 
 export const { showModal, hideModal, showLoading, hideLoading } = commonSlice.actions
