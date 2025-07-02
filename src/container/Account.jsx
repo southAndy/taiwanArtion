58bf4442aa414fb3c844ceb84dac9a3103ab89b6
southAndy <zatchbell.1206@gmail.com>
2025-07-02 22:59:50 +0800

    chore(auth): temporarily disable third-party login

--- src/container/Account.jsx
+++ src/container/Account.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { Link, Outlet, useNavigate } from 'react-router-dom'
 import {
-   loginBannerMobile,
-   facebookIcon,
-   lineIcon,
-   googleIcon,
-   logoWhite,
+  loginBannerMobile,
+  facebookIcon,
+  lineIcon,
+  googleIcon,
+  logoWhite,
 } from '../assets/images/index'
 import Header from './Header/Header'
 import styled from 'styled-components'
@@ -14,25 +14,25 @@ import { PositionElement } from '../styles/base/PositionElement'
 import { breakpoint } from '../styles/utils/breakpoint'
 
 const LoginPage = () => {
-   const navigate = useNavigate()
-   return (
-      <>
-         <Header />
-         <StyledLoginContainer>
-            <StyledBanner>
-               <img src={loginBannerMobile} alt='' />
-               <FixedImage position={'absolute'} left={'50%'} top={'50%'}>
-                  <img src={logoWhite} alt='' />
-               </FixedImage>
-            </StyledBanner>
-            <StyledContainer>
-               <h3 className='title'>歡迎來到早找展覽</h3>
-               <p className='text-[14px] mt-1'>開始探索下一段展覽之旅吧！</p>
-               <div className='option'>
-                  <StyledLoginLink to={'/login'}>登入</StyledLoginLink>
-                  <StyledLoginLink to={'/register'}>註冊</StyledLoginLink>
-               </div>
-               <div className='society'>
+  const navigate = useNavigate()
+  return (
+    <>
+      <Header />
+      <StyledLoginContainer>
+        <StyledBanner>
+          <img src={loginBannerMobile} alt="" />
+          <FixedImage position={'absolute'} left={'50%'} top={'50%'}>
+            <img src={logoWhite} alt="" />
+          </FixedImage>
+        </StyledBanner>
+        <StyledContainer>
+          <h3 className="title">歡迎來到早找展覽</h3>
+          <p className="text-[14px] mt-1">開始探索下一段展覽之旅吧！</p>
+          <div className="option">
+            <StyledLoginLink to={'/login'}>登入</StyledLoginLink>
+            <StyledLoginLink to={'/register'}>註冊</StyledLoginLink>
+          </div>
+          {/* <div className='society'>
                   <p className='mb-4'>或者使用以下帳號登入/註冊</p>
                   <div className='society-option'>
                      <StyledImageBox>
@@ -45,101 +45,101 @@ const LoginPage = () => {
                         <img src={lineIcon} alt='Line圖示' />
                      </StyledImageBox>
                   </div>
-               </div>
-            </StyledContainer>
-         </StyledLoginContainer>
-      </>
-   )
+               </div> */}
+        </StyledContainer>
+      </StyledLoginContainer>
+    </>
+  )
 }
 
 const StyledLoginContainer = styled.div`
-   display: flex;
-   flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-   @media (min-width: ${1024}px) {
-      flex-direction: row;
-   }
+  @media (min-width: ${1024}px) {
+    flex-direction: row;
+  }
 `
 
 const StyledBanner = styled.div`
-   position: relative;
-   height: 200px;
+  position: relative;
+  height: 200px;
 
-   @media (min-width: ${breakpoint.tablet}px) {
-      height: 40vh;
-      object-fit: contain;
-   }
-   @media (min-width: 1024px) {
-      height: 100vh;
-      width: 50%;
-   }
+  @media (min-width: ${breakpoint.tablet}px) {
+    height: 40vh;
+    object-fit: contain;
+  }
+  @media (min-width: 1024px) {
+    height: 100vh;
+    width: 50%;
+  }
 `
 
 const FixedImage = styled(PositionElement)`
-   width: 216px;
-   height: 80px;
-   transform: translate(-50%, -50%);
+  width: 216px;
+  height: 80px;
+  transform: translate(-50%, -50%);
 `
 
 const StyledContainer = styled.div`
-   padding: 24px;
+  padding: 24px;
 
-   .title {
-      color: #be8152;
-      font-size: 18px;
-      font-weight: 700;
-      margin: unset;
-      margin-bottom: 8px;
-   }
-   .option {
-      ${Flex}
-      flex-direction:column;
-      gap: 16px;
-      margin-top: 40px;
-   }
+  .title {
+    color: #be8152;
+    font-size: 18px;
+    font-weight: 700;
+    margin: unset;
+    margin-bottom: 8px;
+  }
+  .option {
+    ${Flex}
+    flex-direction:column;
+    gap: 16px;
+    margin-top: 40px;
+  }
 
-   .society {
+  .society {
+    ${Flex};
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 40px;
+    gap: 24px;
+    color: #535353;
+    cursor: pointer;
+
+    &-title {
+      margin-bottom: 16px;
+    }
+    &-option {
       ${Flex};
-      flex-direction: column;
       justify-content: center;
       align-items: center;
-      margin-top: 40px;
       gap: 24px;
-      color: #535353;
       cursor: pointer;
-
-      &-title {
-         margin-bottom: 16px;
-      }
-      &-option {
-         ${Flex};
-         justify-content: center;
-         align-items: center;
-         gap: 24px;
-         cursor: pointer;
-      }
-   }
-   @media (min-width: ${1024}px) {
-      width: 50%;
-      height: 100vh;
-   }
+    }
+  }
+  @media (min-width: ${1024}px) {
+    width: 50%;
+    height: 100vh;
+  }
 `
 
 const StyledImageBox = styled.div`
-   height: 40px;
-   width: 40px;
+  height: 40px;
+  width: 40px;
 `
 
 const StyledLoginLink = styled(Link)`
-   border: 2px solid #a9622a;
-   border-radius: 20px;
-   padding: 16px;
-   color: #a9622a;
-   text-align: center;
-   font-weight: 700;
-   &:hover {
-      background: #a9622a;
-      color: #fff;
-   }
+  border: 2px solid #a9622a;
+  border-radius: 20px;
+  padding: 16px;
+  color: #a9622a;
+  text-align: center;
+  font-weight: 700;
+  &:hover {
+    background: #a9622a;
+    color: #fff;
+  }
 `
 export default LoginPage
--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -6,12 +6,12 @@ import { setIsLogin } from '../store/memberSlice'
 import Header from './Header/Header'
 import styled from 'styled-components'
 import {
-   hotBg,
-   vectorIcon,
-   facebookIcon,
-   lineIcon,
-   googleIcon,
-   warnIcon,
+  hotBg,
+  vectorIcon,
+  facebookIcon,
+  lineIcon,
+  googleIcon,
+  warnIcon,
 } from '../assets/images/index'
 // import Input from '../components/Input/Input'
 import StyledInput from '../components/StyledInput'
@@ -26,143 +26,140 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import BaseImageBox from '../styles/base/BaseImageBox'
 
 const Login = () => {
-   const [username, setUsername] = useState('')
-   const [password, setPassword] = useState('')
-   //todo 調整 button 元件 props
-   const navigate = useNavigate()
-   const location = useLocation()
-   const dispatch = useDispatch()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  //todo 調整 button 元件 props
+  const navigate = useNavigate()
+  const location = useLocation()
+  const dispatch = useDispatch()
 
-   const schema = yup.object().shape({
-      email: yup.string().email('請輸入有效的信箱格式').required('此欄位為必填'),
-      password: yup.string().required('此欄位為必填'),
-   })
-   const {
-      handleSubmit,
-      register,
-      formState: { errors },
-      setError,
-   } = useForm({
-      resolver: yupResolver(schema),
-      mode: 'onBlur',
-   })
+  const schema = yup.object().shape({
+    email: yup.string().email('請輸入有效的信箱格式').required('此欄位為必填'),
+    password: yup.string().required('此欄位為必填'),
+  })
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setError,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onBlur',
+  })
 
-   async function getUserInfo(uid) {
-      try {
-         const userDatas = doc(db, 'users', uid)
-         const docSnap = await getDoc(userDatas)
-         // 存入 redux
-         dispatch({ type: 'member/setMemberInfo', payload: docSnap.data() })
-      } catch (e) {
-         console.log(e)
-      }
-   }
+  async function getUserInfo(uid) {
+    try {
+      const userDatas = doc(db, 'users', uid)
+      const docSnap = await getDoc(userDatas)
+      // 存入 redux
+      dispatch({ type: 'member/setMemberInfo', payload: docSnap.data() })
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-   const sendLoginRequest = async (data) => {
-      const { email, password } = data
-      try {
-         // 使用信箱和密碼進行驗證
-         const loginInfo = await signInWithEmailAndPassword(auth, email, password)
+  const sendLoginRequest = async data => {
+    const { email, password } = data
+    try {
+      // 使用信箱和密碼進行驗證
+      const loginInfo = await signInWithEmailAndPassword(auth, email, password)
 
-         // 取得使用者資料
-         await getUserInfo(loginInfo.user.uid)
+      // 取得使用者資料
+      await getUserInfo(loginInfo.user.uid)
 
-         // 登入成功後，存 accessToken 到 cookie 中，並將登入狀態改為 true
-         document.cookie = 'accessToken=' + loginInfo.user.accessToken
-         document.cookie = `isLogin=true`
+      // 登入成功後，存 accessToken 到 cookie 中，並將登入狀態改為 true
+      document.cookie = 'accessToken=' + loginInfo.user.accessToken
+      document.cookie = `isLogin=true`
 
-         // 如果是從其他頁面跳轉的，回去該頁面
-         const from = location.state?.from || '/backstage'
-         navigate(from)
-         dispatch({ type: 'member/setIsLogin', payload: true })
-      } catch (error) {
-         console.error('Error during login:', error)
-      }
-   }
+      // 如果是從其他頁面跳轉的，回去該頁面
+      const from = location.state?.from || '/backstage'
+      navigate(from)
+      dispatch({ type: 'member/setIsLogin', payload: true })
+    } catch (error) {
+      console.error('Error during login:', error)
+    }
+  }
 
-   const loginLine = () => {
-      const channel_id = '2003812489'
-      const homePage = 'https://2439-111-241-166-18.ngrok-free.app/?isLogin=true'
-      const lineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${channel_id}&redirect_uri=${homePage}&state=12345abcde&scope=profile&nonce=09876xyz`
+  const loginLine = () => {
+    const channel_id = '2003812489'
+    const homePage = 'https://2439-111-241-166-18.ngrok-free.app/?isLogin=true'
+    const lineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${channel_id}&redirect_uri=${homePage}&state=12345abcde&scope=profile&nonce=09876xyz`
 
-      //登入成功後，將登入狀態改為 true ，並且存入 redux
-      dispatch(setIsLogin(true))
-      window.location.href = lineLoginUrl
-      //將登入成功存入 cookie
-      document.cookie = 'isLogin=true'
-   }
-   const loginGoogle = async () => {
-      try {
-         const result = await signInWithPopup(auth, provider)
-         const user = result.user
-         console.log(user)
-         // todo 增加成員的處理
-         navigate('/')
-      } catch (e) {
-         console.log(e)
-      }
-   }
-   return (
-      <>
-         <Header />
-         <StyledLoginBanner>
-            <StyledLink to='/account'>
-               <img src={vectorIcon} alt='回到上一頁箭頭' />
-            </StyledLink>
-            <StyledTitle className='text-md'>會員登入</StyledTitle>
-         </StyledLoginBanner>
-         <StyledContent>
-            <form onSubmit={handleSubmit(sendLoginRequest)}>
-               <div className='flex flex-col'>
-                  <label htmlFor='email' className='font-medium mb-2'>
-                     電子郵件
-                  </label>
-                  <StyledInput
-                     {...register('email')}
-                     placeholder='請輸入您的電子郵件'
-                  ></StyledInput>
-                  {errors.email ? (
-                     <StyledErrorBox>
-                        <BaseImageBox width={'20px'} height={'20px'}>
-                           <img src={warnIcon} alt='' />
-                        </BaseImageBox>
-                        <span className='text-[#D31C1C]'>{errors.email?.message}</span>
-                     </StyledErrorBox>
-                  ) : (
-                     ''
-                  )}
-               </div>
-               <div className='flex flex-col'>
-                  <label htmlFor='password' className='mb-2 font-medium'>
-                     密碼
-                  </label>
-                  <StyledInput
-                     {...register('password')}
-                     type={'password'}
-                     placeholder='6-18位數密碼,請區分大小寫'
-                  ></StyledInput>
-                  {errors.password ? (
-                     <StyledErrorBox>
-                        <BaseImageBox width={'20px'} height={'20px'}>
-                           <img src={warnIcon} alt='' />
-                        </BaseImageBox>
-                        <span className='text-[#D31C1C]'>{errors.password?.message}</span>
-                     </StyledErrorBox>
-                  ) : (
-                     ''
-                  )}
-               </div>
-               {/* <StyledForgetLink to='/forget-password'>忘記密碼？</StyledForgetLink> */}
-               <Button buttonType={'submit'} content={'登入'} />
-            </form>
-            <section className='remind'>
-               <div>
-                  <span>還不是會員？</span>
-                  <Link to='/register' className='text-[#A9622A]'>
-                     註冊帳號
-                  </Link>
-               </div>
-               <p className=' cursor-pointer'>或者使用以下方式登入</p>
+    //登入成功後，將登入狀態改為 true ，並且存入 redux
+    dispatch(setIsLogin(true))
+    window.location.href = lineLoginUrl
+    //將登入成功存入 cookie
+    document.cookie = 'isLogin=true'
+  }
+  const loginGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+      console.log(user)
+      // todo 增加成員的處理
+      navigate('/')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  return (
+    <>
+      <Header />
+      <StyledLoginBanner>
+        <StyledLink to="/account">
+          <img src={vectorIcon} alt="回到上一頁箭頭" />
+        </StyledLink>
+        <StyledTitle className="text-md">會員登入</StyledTitle>
+      </StyledLoginBanner>
+      <StyledContent>
+        <form onSubmit={handleSubmit(sendLoginRequest)}>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="font-medium mb-2">
+              電子郵件
+            </label>
+            <StyledInput {...register('email')} placeholder="請輸入您的電子郵件"></StyledInput>
+            {errors.email ? (
+              <StyledErrorBox>
+                <BaseImageBox width={'20px'} height={'20px'}>
+                  <img src={warnIcon} alt="" />
+                </BaseImageBox>
+                <span className="text-[#D31C1C]">{errors.email?.message}</span>
+              </StyledErrorBox>
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="mb-2 font-medium">
+              密碼
+            </label>
+            <StyledInput
+              {...register('password')}
+              type={'password'}
+              placeholder="6-18位數密碼,請區分大小寫"
+            ></StyledInput>
+            {errors.password ? (
+              <StyledErrorBox>
+                <BaseImageBox width={'20px'} height={'20px'}>
+                  <img src={warnIcon} alt="" />
+                </BaseImageBox>
+                <span className="text-[#D31C1C]">{errors.password?.message}</span>
+              </StyledErrorBox>
+            ) : (
+              ''
+            )}
+          </div>
+          {/* <StyledForgetLink to='/forget-password'>忘記密碼？</StyledForgetLink> */}
+          <Button buttonType={'submit'} content={'登入'} />
+        </form>
+        <section className="remind">
+          <div>
+            <span>還不是會員？</span>
+            <Link to="/register" className="text-[#A9622A]">
+              註冊帳號
+            </Link>
+          </div>
+          {/* <p className=' cursor-pointer'>或者使用以下方式登入</p>
                <div className='society'>
                   <StyledImageBox>
                      <img src={facebookIcon} alt='' />
@@ -173,84 +170,84 @@ const Login = () => {
                   <StyledImageBox onClick={loginLine}>
                      <img src={lineIcon} alt='' />
                   </StyledImageBox>
-               </div>
-            </section>
-         </StyledContent>
-      </>
-   )
+               </div> */}
+        </section>
+      </StyledContent>
+    </>
+  )
 }
 const StyledErrorBox = styled.div`
-   display: flex;
-   align-items: center;
-   gap: 8px;
-   margin-top: 4px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-top: 4px;
 `
 
 const StyledLink = styled(Link)`
-   width: 18px;
-   height: 18px;
+  width: 18px;
+  height: 18px;
 `
 const StyledForgetLink = styled(Link)`
-   color: #a9622a;
-   width: 85px;
-   margin-left: auto;
+  color: #a9622a;
+  width: 85px;
+  margin-left: auto;
 `
 
 const StyledLoginBanner = styled.section`
-   display: flex;
-   align-items: center;
-   justify-content: start;
-   gap: 90px;
-   padding: 20px;
-   background-image: url(${hotBg});
-   height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 90px;
+  padding: 20px;
+  background-image: url(${hotBg});
+  height: 100px;
 `
 const StyledTitle = styled.h3`
-   font-size: 18px;
-   font-weight: 700;
+  font-size: 18px;
+  font-weight: 700;
 `
 const StyledContent = styled.section`
-   border-radius: 16px;
-   padding: 24px;
+  border-radius: 16px;
+  padding: 24px;
 
-   form {
-      display: flex;
-      flex-direction: column;
-      gap: 16px;
-      margin-bottom: 40px;
-   }
-   button {
-      border-radius: 20px;
-      border: none;
-      background-color: #eeeeee;
-      color: #5f5f5f;
-      padding: 9px 0;
-      cursor: pointer;
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    margin-bottom: 40px;
+  }
+  button {
+    border-radius: 20px;
+    border: none;
+    background-color: #eeeeee;
+    color: #5f5f5f;
+    padding: 9px 0;
+    cursor: pointer;
 
-      &:hover {
-         background-color: #a9622a;
-         color: white;
-      }
-   }
-   .remind {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 16px;
+    &:hover {
+      background-color: #a9622a;
+      color: white;
+    }
+  }
+  .remind {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
 
-      a {
-         color: #a9622a;
-      }
-   }
-   .society {
-      display: flex;
-      gap: 24px;
-   }
+    a {
+      color: #a9622a;
+    }
+  }
+  .society {
+    display: flex;
+    gap: 24px;
+  }
 `
 const StyledImageBox = styled.div`
-   cursor: pointer;
-   height: 40px;
-   width: 40px;
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
 `
 
 export default Login
