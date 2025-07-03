b0d4a29a88ffb807658177cb3694aa11c2631887
southAndy <zatchbell.1206@gmail.com>
2025-07-03 16:13:03 +0800

    refactor(App):modify redux function's position

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
-import store from './store'
 import './assets/scss/main.scss'
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<App />)
+  ReactDOM.createRoot(rootElement).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
 }
