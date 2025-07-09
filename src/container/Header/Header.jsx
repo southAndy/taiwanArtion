5db0325eb2a9632f02b38da22f8b496a9cceaec9
southAndy <zatchbell.1206@gmail.com>
2025-07-09 16:40:47 +0800

    feat(header):fix header in top

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,6 +34,11 @@ const Header = () => {
 }
 
 const HeaderContainer = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
   display: flex;
   justify-content: space-between;
   align-items: center;
