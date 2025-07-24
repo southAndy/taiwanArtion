27b4d81d325fcc42c998d3977b3f1dfa9cb43f0f
southAndy <zatchbell.1206@gmail.com>
2025-07-24 10:41:30 +0800

    refactor(eslint): reorganize rules and add strict code quality

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,16 +20,19 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
+    'react/jsx-key': 'error', // force use key attribute
+    'react/jsx-no-target-blank': 'warn',
+    'react/no-unescaped-entities': 'warn',
+
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // not allow unused vars
     'no-console': ['error', { allow: ['warn', 'error'] }], // not allow console.log
-    'no-debugger': 'error',
+    'no-debugger': 'error', // not allow debugger
 
     'import/no-unresolved': 'warn',
-    'react/jsx-key': 'error', // force use key attribute
     'no-undef': 'error', // not allow undefined variables
     'no-empty': 'warn',
-    'react/no-unescaped-entities': 'warn',
-    'react/jsx-no-target-blank': 'warn',
+    eqeqeq: 'error', // not allow == or !=
+    'no-var': 'error', // not allow var
   },
   settings: {
     react: {
