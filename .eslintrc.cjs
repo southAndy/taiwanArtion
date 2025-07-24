b3d229df980ff77bad288456b0e28a9112bc1e21
southAndy <zatchbell.1206@gmail.com>
2025-07-24 10:25:35 +0800

    doc(eslint):translate comment to English

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:import/recommended',
-    'prettier', // 必須放在最後，用來關閉與 Prettier 衝突的規則
+    'prettier', // must be at the end, to close the rules that conflict with Prettier
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -18,17 +18,15 @@ module.exports = {
   },
   plugins: ['react', 'import'],
   rules: {
-    // React 17+ 不需要 import React
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off', // PropTypes 設為警告
-    // 允許未使用的變數（以 _ 開頭）
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': ['error', { allow: ['warn', 'error'] }], // 不允許使用 console.log
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // not allow unused vars
+    'no-console': ['error', { allow: ['warn', 'error'] }], // not allow console.log
     'no-debugger': 'error',
-    // todo: 臨時將錯誤降級為警告，讓 CI 通過
+
     'import/no-unresolved': 'warn',
-    'react/jsx-key': 'error', // 強制使用 key 屬性
-    'no-undef': 'error', // 不允許未定義的變數
+    'react/jsx-key': 'error', // force use key attribute
+    'no-undef': 'error', // not allow undefined variables
     'no-empty': 'warn',
     'react/no-unescaped-entities': 'warn',
     'react/jsx-no-target-blank': 'warn',
